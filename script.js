4817adee7bd0b8386347b880887433e72d967050
Roozeppe Jean Pierre <roozeppe.jp@gmail.com>
2016-05-04 16:18:21 -0400

    Major upgrade

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-var img = new Image();
-img.src = 'fry.jpg';
-img.onload = function() {
-  draw(this);
-};
-
-function draw(img) {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-  var imageData = ctx.getImageData(0, 0, 500, 500);
-  var data = imageData.data;
+var handleFileSelect = function(evt) {
+  var canvasWidth = 500;
+  var canvasHeight = 500;
+  var file = evt.target.files[0];
+      
+  var reader = new FileReader();
+  reader.onload = function(fileObject) {
+    var data = fileObject.target.result;
+        
+    // Create an image object
+    var image = new Image();
+    image.onload = function() {
+          
+      window.imageSrc = this;
+      draw(window.imageSrc);
+    }
+        
+    // Set image data to background image.
+    image.src = data;
+  };
+  reader.readAsDataURL(file)
+}
     
+var draw = function(img) {
+  // Get Canvas2DContext
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext("2d");
+
+  // Check if image exists.
+  if (img != null) {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  }
+
+  img.style.display = 'none';
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+  // Function that inverts the image.
   var invert = function() {
     for (var i = 0; i < data.length; i += 4) {
       data[i]     = 255 - data[i];     // red
@@ -21,6 +45,7 @@ function draw(img) {
     ctx.putImageData(imageData, 0, 0);
   };
 
+  // Function that grayscales the image.
   var grayscale = function() {
     for (var i = 0; i < data.length; i += 4) {
       var avg = (data[i] + data[i +1] + data[i +2]) / 3;
@@ -31,8 +56,17 @@ function draw(img) {
     ctx.putImageData(imageData, 0, 0);
   };
 
-  var invertbtn = document.getElementById('invertbtn');
+  // Listen for a button's event and apply appropriate grayscaling.
+  var invertbtn = document.getElementById('invertBtn');
   invertbtn.addEventListener('click', invert);
-  var grayscalebtn = document.getElementById('grayscalebtn');
+  var grayscalebtn = document.getElementById('grayscaleBtn');
   grayscalebtn.addEventListener('click', grayscale);
+
+  // Listen for a button's event and save file. 
+  var saveFile =function() {
+    window.open(document.querySelector('canvas').toDataURL());
+  }
+  document.querySelector('#saveBtn').addEventListener('click', saveFile, false);
 }
+   
+document.getElementById('file').addEventListener('change', handleFileSelect, false);
